03124375db75d6f3d0f4d2ff3b859e88917bee62
unknown <jeka20001980@gmail.com>
2024-04-11 10:03:46 +0200

    add bankAccountController

--- src/controllers/BankAccountController.ts
+++ src/controllers/BankAccountController.ts
@@ -1,5 +1,6 @@
 import ApiError from "../helpers/ApiErrors.js";
 import { BankAccount, BankAccountAttributes } from "../models/BankAccount.js";
+import { Request, Response, NextFunction } from "express";
 import { Client } from "../models/Client.js";
 
 class BankAccountController {
@@ -13,6 +14,64 @@ class BankAccountController {
       });
     }
     return await BankAccount.bulkCreate(bankAccountValues);
+  }
+
+  async getBankAccount(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const bankAccount = await BankAccount.findByPk(id);
+      if (!bankAccount) {
+        return next(ApiError.notFound("Bank account not found"));
+      }
+      res.json(bankAccount);
+    } catch (error) {
+      console.log(error);
+      return next(ApiError.internal("Error getting bank account"));
+    }
+  }
+
+  async createBankAccount(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { balance, clientId, departmentIdWhereOpen } = req.body;
+
+      if (!balance || !clientId || !departmentIdWhereOpen) {
+        return next(ApiError.badRequest("balance, clientId, departmentIdWhereOpen are required"));
+      }
+
+      const bankAccount = await BankAccount.create({
+        balance,
+        clientId,
+        departmentIdWhereOpen,
+      });
+
+      res.json(bankAccount);
+    } catch (error) {
+      console.log(error);
+      return next(ApiError.internal("Error creating bank account"));
+    }
+  }
+
+  async updateBankAccount(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { balance, clientId, departmentIdWhereOpen } = req.body;
+
+      const bankAccount = await BankAccount.findByPk(id);
+      if (!bankAccount) {
+        return next(ApiError.notFound("Bank account not found"));
+      }
+
+      await bankAccount.update({
+        balance,
+        clientId,
+        departmentIdWhereOpen,
+      });
+
+      res.json(bankAccount);
+    } catch (error) {
+      console.log(error);
+      return next(ApiError.internal("Error updating bank account"));
+    }
   }
 }
 
