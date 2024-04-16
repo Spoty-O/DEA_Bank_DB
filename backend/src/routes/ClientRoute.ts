c6510f4cacfe479d2acf0a1efc9dad21cab40abc
unknown <jeka20001980@gmail.com>
2024-04-16 12:57:18 +0200

    fix

--- backend/src/routes/ClientRoute.ts
+++ backend/src/routes/ClientRoute.ts
@@ -3,7 +3,7 @@ import ClientController from "../controllers/ClientController.js";
 const clientRouter = Router();
 
 clientRouter.get("/:id", ClientController.getClientById);
-clientRouter.get("/:firstName&:lastName", ClientController.getClientByName);
+clientRouter.get("/find/:firstName&:lastName", ClientController.getClientByName);
 clientRouter.get("/", ClientController.getAllClients);
 clientRouter.post("/", ClientController.createClient);
 clientRouter.patch("/:id", ClientController.updateClient);
