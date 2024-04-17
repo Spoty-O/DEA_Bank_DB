2c5c4db8c6d3c790b3e0ee08e1614a99492f1f69
unknown <korotysh16@ukr.net>
2024-04-17 14:16:54 +0300

    route added

--- backend/src/routes/ReplicationRoute.ts
+++ backend/src/routes/ReplicationRoute.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import ReplicationController from "../controllers/ReplicationController.js";
+import DepartmentController from "../controllers/DepartmentController.js";
 const replicationRouter = Router();
 
-replicationRouter.get("/client", ReplicationController.getUserByName, ReplicationController.createReplication);
+replicationRouter.get("/client/:firstName&:lastName", DepartmentController.getDepartments, ReplicationController.getUserByName, ReplicationController.createReplication);
 
 export default replicationRouter;
